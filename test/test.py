1880125cff9f04e66d8f1f4e2b8f08d46a7514e1
Breno <brenomessias@yahoo.com>
2026-09-19 13:58:30 -0300

    Update test.py

--- test/test.py
+++ test/test.py
@@ -2,26 +2,39 @@ from src.main import *
 from unittest.mock import patch
 
 def test_root():
-    assert root() == {"message": "Meu primeiro EndPoint!"}
+    result = root()
+    yield result
+    assert result == {"message": "Meu primeiro EndPoint!"}
 
 def test_segundoendpoint():
     with patch('random.randint', return_value=123):
         result = segundoendpoint()
+        yield result
 
     assert result == {"teste":True, "num_random": 123}
 
 def test_create_estudante():
     estudante_teste = Estudante(name="Fulano", curso="Curso 1", ativo=False)
-    assert estudante_teste == create_estudante(estudante_teste)
+    result = create_estudante(estudante_teste)
+    yield result
+    assert estudante_teste == result
 
 def test_update_estudante_negativo():
-    assert not update_estudante(-5)
+    result = update_estudante(-5)
+    yield result
+    assert not result
 
 def test_update_estudante_positivo():
-    assert update_estudante(10)
+    result = update_estudante(10)
+    yield result
+    assert result
 
 def test_delete_estudante_negativo():
-    assert not delete_estudante(-5)
+    result = delete_estudante(-5)
+    yield result
+    assert not result
 
 def test_delete_estudante_positivo():
-    assert delete_estudante(1)
+    result = delete_estudante(1)
+    yield result
+    assert result
